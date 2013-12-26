514fe2be5642fd811859ea21624b26bea9f8b6ab
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2013-12-26 12:49:59 +0000

    Tidying up

--- site.hs
+++ site.hs
@@ -1,48 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
 import Hakyll
-import Data.List (sortBy,isSuffixOf)
+import Data.List (isSuffixOf)
 import System.FilePath.Posix (takeBaseName,takeDirectory,(</>))
 
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route idRoute
-        compile copyFileCompiler
-
     match "css/*" $ do
         route idRoute
         compile compressCssCompiler
 
-    match "404.html" $ do
-        route idRoute
-        compile $ do
-            getResourceBody
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-
     match "posts/*" $ do
         route $ niceRoute
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    match "henri/*" $ do
-        route $ niceRoute
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/basic.html" postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            let indexCtx = field "posts" $ \_ ->
-                                postList $ return -- recentFirst
+            let indexCtx = field "posts" $ \_ -> postList
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
                 >>= cleanIndexUrls
 
@@ -53,27 +34,20 @@ niceRoute :: Routes
 niceRoute = customRoute createIndexRoute
   where
     createIndexRoute ident = takeDirectory p
---                                 </> drop 11 (takeBaseName p)
                                  </> takeBaseName p
                                  </> "index.html"
                            where p = toFilePath ident
 
 cleanIndexUrls :: Item String -> Compiler (Item String)
 cleanIndexUrls = return . fmap (withUrls clean)
-    where
-        idx = "index.html"
-        clean url
-            | idx `isSuffixOf` url = take (length url - length idx) url
-            | otherwise = url
+  where clean = (`removeTrailing` "index.html")
 
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+removeTrailing :: Eq a => [a] -> [a] -> [a]
+removeTrailing xs suf | suf `isSuffixOf` xs = take (length xs - length suf) xs
+                      | otherwise = xs
 
-postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
-postList sortFilter = do
-    posts <- sortFilter =<< loadAll "posts/*"
+postList :: Compiler String
+postList = do
+    posts <- loadAll "posts/*"
     itemTpl <- loadBody "templates/post-item.html"
-    list <- applyTemplateList itemTpl postCtx posts
-    return list
+    applyTemplateList itemTpl defaultContext posts
