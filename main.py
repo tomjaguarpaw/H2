5fd0ad9f630dd7ecdbaba5af50ac34b049573d33
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-04-10 12:51:18 +0100

    Basic main file

---
+++ main.py
@@ -0,0 +1,19 @@
+def foo1():
+    print("foo1")
+
+def foo2():
+    print("foo2")
+
+def foo3():
+    print("foo3")
+
+def foo4():
+    print("foo4")
+
+def foo5():
+    print("foo5")
+
+def main():
+    foo1()
+    foo2()
+    foo3()
