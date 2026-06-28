f9f294dd687f44a8a301901b58a5535a46b9ebef
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-06-28 13:29:06 +0100

    (change) Add list of top articles

--- site.hs
+++ site.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.List (isSuffixOf)
+import Data.String (fromString)
 import Hakyll
 import System.FilePath.Posix (takeBaseName, takeDirectory, (</>))
 import Text.Pandoc.Options
@@ -23,7 +24,8 @@ main = hakyll $ do
     route idRoute
     compile $ do
       let indexCtx =
-            field "posts" $ \_ -> postList
+            foldMap (\(name, list) -> field name (\_ -> list)) topPostList
+              <> field "posts" (\_ -> postList)
 
       getResourceBody
         >>= applyAsTemplate indexCtx
@@ -65,3 +67,59 @@ postList = do
   posts <- loadAll "posts/*"
   itemTpl <- loadBody "templates/post-item.html"
   applyTemplateList itemTpl defaultContext posts
+
+topPostList :: [(String, Compiler String)]
+topPostList =
+  [ ( "bluefinPosts",
+      foo
+        [ "bluefin-all",
+          "bluefin-plucking-constraints",
+          "bluefin-prevents-handles-leaking",
+          "bluefin-streams-finalize-promptly",
+          "bluefin-versus-oop"
+        ]
+    ),
+    ( "mathsAndPhysicsPosts",
+      foo
+        [ "bells-theorem-made-simpler",
+          "symbolic-expressions-can-be-automatically-differentiated",
+          "why-is-naive-symbolic-differentiation-slow",
+          "automatic-differentiation-worked-examples",
+          "automatic-differentiation-worked-examples-maths"
+        ]
+    ),
+    ( "haskellPosts",
+      foo
+        [ "haskells-missing-mutable-ref",
+          "ioscopedref-reference-implementation",
+          "fork-fragile-reader-like-operations",
+          "nested-strict-data",
+          "data-map-strict-map-not-strict-map",
+          "impure-lazy-language",
+          "make-invalid-laziness-unrepresentable",
+          "foldl-traverses-state-foldr-traverses-anything",
+          "scrap-your-iteration-combinators",
+          "ghc-8.10-9.6-experience-report",
+          "opaleyes-api-breakage-policy",
+          "demystifying-dlist",
+          "exitfailure-doesnt-exit",
+          "good-design-and-type-safety-in-yahtzee",
+          "using-brain-less-refactoring-yahtzee",
+          "hascallstack-domain-errors",
+          "improving-the-typed-process-documentation",
+          "mysterious-incomposability-of-decidable"
+        ]
+    ),
+    ( "programmingPosts",
+      foo
+        [ "git-rebase-conflicts",
+          "git-survival-guide"
+        ]
+    )
+  ]
+
+foo :: [String] -> Compiler String
+foo postNames = do
+  posts <- mapM (load . fromString . ("posts/" <>) . (<> ".md")) postNames
+  tpl <- loadBody "templates/post-item.html"
+  applyTemplateList tpl defaultContext posts
