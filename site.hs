34fd27946a23b72e641e96e3c93678302467756c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-06-28 09:31:21 +0100

    (whitespace) Ormolu

--- site.hs
+++ site.hs
@@ -1,60 +1,68 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Hakyll
-import Text.Pandoc.Options
+
 import Data.List (isSuffixOf)
-import System.FilePath.Posix (takeBaseName,takeDirectory,(</>))
+import Hakyll
+import System.FilePath.Posix (takeBaseName, takeDirectory, (</>))
+import Text.Pandoc.Options
 
 main :: IO ()
 main = hakyll $ do
-    match "css/*" $ do
-        route idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match "posts/*" $ do
-        route directoryRoute
-        compile $ pandocCompilerWith defaultHakyllReaderOptions defaultHakyllWriterOptions{ writerHTMLMathMethod = MathJax "" }
-            >>= loadAndApplyTemplate "templates/post.html" defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route directoryRoute
+    compile $
+      pandocCompilerWith defaultHakyllReaderOptions defaultHakyllWriterOptions {writerHTMLMathMethod = MathJax ""}
+        >>= loadAndApplyTemplate "templates/post.html" defaultContext
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            let indexCtx = field "posts" $ \_ -> postList
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      let indexCtx = field "posts" $ \_ -> postList
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html"
-                         ((field "title" $ \_ -> return "The H2 Wiki")
-                          <> defaultContext)
-                >>= relativizeUrls
-                >>= cleanIndexUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate
+          "templates/default.html"
+          ( (field "title" $ \_ -> return "The H2 Wiki")
+              <> defaultContext
+          )
+        >>= relativizeUrls
+        >>= cleanIndexUrls
 
-    match "templates/*" $ compile templateCompiler
+  match "templates/*" $ compile templateCompiler
 
-    match "images/*" $ do
-      route idRoute
-      compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
 indexRoute :: Identifier -> FilePath
-indexRoute ident = takeDirectory p
-                   </> takeBaseName p
-                   </> "index.html"
-  where p = toFilePath ident
+indexRoute ident =
+  takeDirectory p
+    </> takeBaseName p
+    </> "index.html"
+  where
+    p = toFilePath ident
 
 directoryRoute :: Routes
 directoryRoute = customRoute indexRoute
 
 cleanIndexUrls :: Item String -> Compiler (Item String)
 cleanIndexUrls = return . fmap (withUrls clean)
-  where clean = (`removeTrailing` "index.html")
+  where
+    clean = (`removeTrailing` "index.html")
 
-removeTrailing :: Eq a => [a] -> [a] -> [a]
-removeTrailing xs suf | suf `isSuffixOf` xs = take (length xs - length suf) xs
-                      | otherwise = xs
+removeTrailing :: (Eq a) => [a] -> [a] -> [a]
+removeTrailing xs suf
+  | suf `isSuffixOf` xs = take (length xs - length suf) xs
+  | otherwise = xs
 
 postList :: Compiler String
 postList = do
-    posts <- loadAll "posts/*"
-    itemTpl <- loadBody "templates/post-item.html"
-    applyTemplateList itemTpl defaultContext posts
+  posts <- loadAll "posts/*"
+  itemTpl <- loadBody "templates/post-item.html"
+  applyTemplateList itemTpl defaultContext posts
